f7fb61ea412407e98673191499e68bfc62a27558
kalyogi <Yogeshbhopale4@gmail.com>
2024-08-11 21:43:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb://cokenclips:<yyyyyyy>@cokenclips.7mq3z.mongodb.net/?retryWrites=true&w=majority&appName=Cokenclips")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cokenclips:<yyyyyyy>@cokenclips.7mq3z.mongodb.net/?retryWrites=true&w=majority&appName=Cokenclips")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cokenclipsdb")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'cokenclips')
 
